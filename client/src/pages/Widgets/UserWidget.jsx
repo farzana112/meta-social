6d3d383548a83122b60f0a7ee0dcab49bbbf8503
farzana <farzanakn0102@gmail.com>
2023-07-12 21:16:38 +0530

    second commit

--- client/src/pages/Widgets/UserWidget.jsx
+++ client/src/pages/Widgets/UserWidget.jsx
@@ -1,8 +1,10 @@
 import {
  ManageAccountsOutlined,
  EditOutlined,
- LocationOutlined,
+ LocationOnOutlined,
  WorkOutlineOutlined,
+ ReportOutlined,
+ MessageOutlined
 } from "@mui/icons-material"
 
 import {
@@ -44,6 +46,8 @@ const getUser= async () =>{
     })
 
     const data=await response.json()
+    console.log("data")
+    console.log(data)
     setUser(data)
 }
 
@@ -63,16 +67,192 @@ const{
 } =user
 
 return(
-    <WidgetWrapper>
-      <FlexBetween
-      gap=".5rem"
-      pb="1.1rem"
-      onClick={()=>navigate(`/profile/${userId}`)}
+    // <WidgetWrapper>
+    //   <FlexBetween
+    //   gap=".5rem"
+    //   pb="1.1rem"
+    //   onClick={()=>navigate(`/profile/${userId}`)}
 
-      >
-        <FlexBetween></FlexBetween>
-        </FlexBetween>  
-    </WidgetWrapper>
+    //   >
+    //     <FlexBetween></FlexBetween>
+    //     </FlexBetween>  
+    // </WidgetWrapper>
+
+    <WidgetWrapper>
+    <FlexBetween gap='0.5rem' pb='1.1rem'>
+      <FlexBetween gap='1rem' onClick={() => navigate(`/profile/${userId}`)}>
+        <UserImage
+          image={ user.displayPicture}
+        />
+        <Box>
+          <Typography
+            variant='h4'
+            color={dark}
+            fontWeight='500'
+            sx={{
+              "&:hover": {
+                color: palette.primary.light,
+                cursor: "pointer",
+              },
+            }}
+          >
+            { user?.name}
+          </Typography>
+          <Typography color={medium}>
+            {user?.userName}
+          </Typography>
+        </Box>
+      </FlexBetween>
+      {isProfile && userId !== user._id && (
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Button variant='outlined' >
+           
+          </Button>
+            <Button
+              variant='outlined'
+              startIcon={<MessageOutlined />}
+              
+              sx={{ marginTop: "0.5rem" }}
+            >
+              Message
+            </Button>
+          
+        </Box>
+      )}
+      {userId === user._id && (
+        <ManageAccountsOutlined
+          sx={{
+            "&:hover": { color: palette.primary.light, cursor: "pointer" },
+          }}
+          
+        />
+      )}
+    </FlexBetween>
+
+   
+    <Divider />
+
+    <Box p='1rem 0'>
+      <Box display='flex' alignItems='center' gap='1rem' mb='0.5rem'>
+        <LocationOnOutlined fontSize='large' sx={{ color: main }} />
+        <Typography color={medium}>
+          { user?.location}
+        </Typography>
+      </Box>
+      <Box display='flex' alignItems='center' gap='1rem'>
+        <WorkOutlineOutlined fontSize='large' sx={{ color: main }} />
+        <Typography color={medium}>
+          { user?.about}
+        </Typography>
+      </Box>
+    </Box>
+
+    <Divider />
+
+    <Box p='1rem 0'>
+      <FlexBetween mb='0.5rem'>
+        <Typography color={main} fontWeight='500'>
+          Followers
+        </Typography>
+        <Typography color={main} fontWeight='500'>
+       0
+        </Typography>
+      </FlexBetween>
+      <FlexBetween>
+        <Typography color={main} fontWeight='500'>
+          Following
+        </Typography>
+        <Typography color={main} fontWeight='500'>
+            0
+        </Typography>
+      </FlexBetween>
+    </Box>
+
+    <Divider />
+
+    <Box p='1rem 0'>
+      <Typography fontSize='1rem' color={main} fontWeight='500' mb='1rem'>
+        Social Profiles
+      </Typography>
+
+      <FlexBetween gap='1rem' mb='0.5rem'>
+        <FlexBetween gap='1rem'>
+          <img src='../assets/twitter.png' alt='twitter' />
+          <Box>
+            <Typography color={main} fontWeight='500'>
+              Twitter
+            </Typography>
+            <Typography color={medium}>Social Network</Typography>
+          </Box>
+        </FlexBetween>
+        <EditOutlined sx={{ color: main }} />
+      </FlexBetween>
+
+      <FlexBetween gap='1rem'>
+        <FlexBetween gap='1rem'>
+          <img src='../assets/linkedin.png' alt='linkedin' />
+          <Box>
+            <Typography color={main} fontWeight='500'>
+              Linkedin
+            </Typography>
+            <Typography color={medium}>Network Platform</Typography>
+          </Box>
+        </FlexBetween>
+        <EditOutlined sx={{ color: main }} />
+      </FlexBetween>
+    </Box>
+
+    <Divider />
+    {isProfile && userId !== user._id && (
+      <>
+        <Box p='1rem 0'>
+          <Box display='flex' justifyContent='center'>
+            <Button
+              variant='outlined'
+              startIcon={<ReportOutlined />}
+              
+            >
+              {reported ? "Already Reported" : "Report"}
+            </Button>
+          </Box>
+        </Box>
+      </>
+    )}
+    <Dialog open={openReportDialog} onClose={handleCloseReportDialog}>
+      <DialogTitle>Report User</DialogTitle>
+      <DialogContent>
+        <TextField
+          autoFocus
+          multiline
+          rows={4}
+          variant='outlined'
+          label='Reason for Reporting'
+          value={reportReason}
+          onChange={handleReportReasonChange}
+          fullWidth
+        />
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={handleCloseReportDialog}>Cancel</Button>
+        <Button
+          onClick={() => {
+            handleReportSubmit(userData ? userData._id : user._id);
+          }}
+          variant='contained'
+          color='primary'
+          disabled={reportReason.trim() === ""}
+        >
+          Submit
+        </Button>
+      </DialogActions>
+    </Dialog>
+  </WidgetWrapper>
 )
 
 }
