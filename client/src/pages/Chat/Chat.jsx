c60ec21410d9f0dee4550a6c72dd3103eae56e1e
farzana <farzanakn0102@gmail.com>
2023-08-13 18:47:45 +0400

    final commit 14

--- client/src/pages/Chat/Chat.jsx
+++ client/src/pages/Chat/Chat.jsx
@@ -27,7 +27,7 @@ const Chat = () => {
 
   // Connect to Socket.io
   useEffect(() => {
-    socket.current = io("http://localhost:5000");
+    socket.current = io("https://metasocial.cloud");
     socket.current.emit("new-user-add", user._id);
     socket.current.on("get-users", (users) => {
       setOnlineUsers(users);
